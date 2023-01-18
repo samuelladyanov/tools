7c9bcd7b420b902e7bab02937e734f5b213e95e8
samuelladyanov <samuel.ladyanov@gmail.com>
2023-01-17 21:05:43 -0800

    funne

--- move_videos.py
+++ move_videos.py
@@ -7,7 +7,9 @@ import mimetypes
 def copy_video(video_dir, video_output):
     '''This function copies video files from a one directory into another.'''
 
-    os.makedirs(sys.argv[2], exist_ok=True)
+    output_path = os.path.join(sys.argv[2], 'copied_videos')
+
+    os.makedirs(output_path, exist_ok=True)
 
     def check_video(video):
         '''Checks if a file has a video extension'''
@@ -24,25 +26,33 @@ def copy_video(video_dir, video_output):
     for dirs, subdirs, files in os.walk(video_dir):
         for file in files:
             if check_video(file) == True:
-                shutil.copy(os.path.join(dirs, file), video_output)
+                shutil.copy(os.path.join(dirs, file), output_path)
 
 
 
 if __name__=='__main__':
 
     if '-h' in sys.argv:
-        print('''                 Enter a full path to your video folder. For output folder, you can have the folder created by specifying a name (ex:'output') and it will save in your working directory. To save to a different path, use the full path followed by "/name_of_new_folder" to generate folder. If name of new folder is not specified, video files will copy into existing directory. NOTE: if your output path isn't an existing path it will create a nested folder in your working directory in the format of the path you entered.  \n 
+        print('''                 Enter a FULL path to your video folder and output folder. Example:( python move_videos.py "your/video/folder" "your/output/folder" ). A folder will be created within your output path with the copied videos inside. \n 
                  Missing Argument: Provide two paths or this error will occur. \n
-                 Invalid folder path: This error occurs when the folder you passed does not exist or the path was not a full path.\n''')
+                 Invalid folder path: This error occurs when the directory you passed does not exist or the path was not a full path.\n''')
    
-    elif len(sys.argv) != 3:
-        print('Missing Argument: 1 or 2 paths are missing (reference -h for help)')
+    elif len(sys.argv) == 1:
+        print('Missing Argument: Video folder path not passed (reference -h for help)')
         exit()
-    
+    elif len(sys.argv) == 2:
+        print('Missing Argument: Output folder path not passed (reference -h for help)')
+        exit()
+        
     else:
         if not os.path.exists(sys.argv[1]):
             print('Invalid video folder path. (reference -h for help)')
             exit()
-        
-            
+        if not os.path.exists(sys.argv[2]):
+            print('Invalid output folder path. (reference -h for help)')
+            exit()
+        if sys.argv[1] == sys.argv[2]:
+            print('DOOFUS DETECTED: BRO JUST PUT IT IN THE SAME FOLDER :skull: WHY ARE YOU EVEN USING THIS?')
+            exit()
+
         copy_video(sys.argv[1], sys.argv[2])
